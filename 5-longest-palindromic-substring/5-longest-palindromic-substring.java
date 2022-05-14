e57f8c1fda09f66d19f54bdd4d88708218ee0591
James Irungu <53642007+jamesirungu@users.noreply.github.com>
2022-05-14 16:25:12 +0300

    Time: 58 ms (49.31%), Space: 43.3 MB (59.10%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.java
@@ -1,4 +1,12 @@
 class Solution {
+    //if the string size is less than 1(null), return the input
+    //loop through the string 
+    //at every index, check whether the value on left and value on right are equal to know if the odd number parindrome scenario exist
+    //if it does, and the right - left is less than r -l , replace right with current r and replace left with current left
+    //decrement left and increment right pointers and loop again
+    //end of loop is when l<0 or r >s.length-1
+    //Next is to check the scenario where palindrome might happend due to repeated characters eg bb
+    //do a similar loop but in initiate value of l to i and r = i+1 and compare then repeat the decrement for l and increment for r comparing to right - left to know if there is a need to update left and right
     public String longestPalindrome(String s) {
         if(s.length()<1)
             return s;
