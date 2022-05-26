861883aa00770d608e4ae785ee733727822615c5
James Irungu <53642007+jamesirungu@users.noreply.github.com>
2022-05-26 23:09:21 +0300

    Time: 1 ms (41.46%), Space: 41.6 MB (10.61%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,3 +1,8 @@
+//int its only one unique way to get to the the finish line from itself.For this reason, the grid can be poulated with 1 in the last cell
+//having only one way, there is only one way to access the cells on the last row and last column based on the fact that from these cells you can only move to one way
+//For every cell, there are two possible moves, either to the right or to the left, so from the left bottom, second last row second last column calcuate paths[i][j]= pathc[i+1][j]+ paths[i][j+1]
+//loop backwords populating values based on this formula.
+//return value on cell paths[0][0]
 class Solution {
     public int uniquePaths(int m, int n) {
         int paths[][] = new int[m][n];
