c522704e1f0fd4274285a7a93f28f0aacd40afef
James Irungu <53642007+jamesirungu@users.noreply.github.com>
2022-05-27 01:32:37 +0300

    Time: 3 ms (53.47%), Space: 46.6 MB (11.82%) - LeetHub

---
+++ 64-minimum-path-sum/64-minimum-path-sum.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int minPathSum(int[][] grid) {
+        int y = grid[0].length-1;
+        int x = grid.length-1;
+        
+        int[][] path = new int[x+1][y+1];
+        path[x][y]=grid[x][y];
+        for(int i=x;i>=0;i--){
+            for(int j=y;j>=0;j--){
+                if(i==x && j<y){
+                    path[i][j]=grid[i][j]+path[i][j+1];
+                }
+                else if(j==y && i<x){
+                    path[i][j]=grid[i][j]+path[i+1][j];
+                }
+                else if(i<x && j<y){
+                    path[i][j]=grid[i][j]+Math.min(path[i+1][j],path[i][j+1]);
+                }
+                else{
+                    path[i][j]=grid[i][j];
+                }
+            }
+        }
+        return path[0][0];
+    }
+}
