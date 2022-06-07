3a8d609a8ddb8b284dcd88d56f984995702423a9
James Irungu <53642007+jamesirungu@users.noreply.github.com>
2022-06-07 20:43:26 +0300

    Time: 0 ms (100.00%), Space: 42.2 MB (36.97%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.java
+++ 54-spiral-matrix/54-spiral-matrix.java
@@ -1,4 +1,10 @@
 class Solution {
+    //navigate to right until there you eithe get to end of the row or find a visited cell in a dp visited matrix
+    //if you find either, move to the cell below
+    //if you get to the cell at the bottom or find a visited cell below, move left
+    //if you get to the left most possibe cell, move up
+    // to ensure you dont start going to the right immediately after moving up one cell, maintain a character showing the previous direction and only move right if previous was up and the next up is marked true
+    // reccursion would help calling the next move
     public List<Integer> spiralOrder(int[][] matrix) {
         return traverse(matrix,new boolean[matrix.length][matrix[0].length],0,0,new ArrayList<>(),'r');
     }
