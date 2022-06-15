5f08210650605037635c3a4989fe4528f9f5186e
James Irungu <53642007+jamesirungu@users.noreply.github.com>
2022-06-15 09:53:54 +0300

    Time: 7 ms (8.11%), Space: 46.8 MB (5.46%) - LeetHub

--- 142-linked-list-cycle-ii/142-linked-list-cycle-ii.java
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.java
@@ -10,6 +10,12 @@
  * }
  */
 public class Solution {
+    /*
+     Initializa a hashmap and keep adding nodes to it as you traverse
+     before adding each node, confrm if it contains the node which is set as key(complexity of contains key is O(1)
+     if it contains the key node, return the node
+     if traversaql is complete with no cases of map containing the key, return null
+    */
     public ListNode detectCycle(ListNode head) {
         ListNode traversalPointer=head;
         Map<ListNode,Integer> visited = new HashMap<>();
